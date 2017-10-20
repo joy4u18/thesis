a279025799b2731dac3c1dcccf784072ff5ad919
joy4u18 <joy4u18@gmail.com>
2017-10-20 20:35:11 +0200

    ver9 edit
    Modified the scripts for ver 9 of the manuscript

---
+++ matlab/auditorymodels/patterson/aim2006/Sounds/PT_1000Hz/PT_1000Hz_project.m
@@ -0,0 +1,23 @@
+
+% Project information
+% for the project: 
+
+%%%%%%%%%%%%%
+%   PT_1000Hz
+%   19-Oct-2017
+%   produced by joy4u
+% Dont write anything in this file
+%%%%%%%%%%%%%
+current_plot=6;
+start_time=6;
+duration=0.010000;
+scale=1.000000;
+hastime=1;
+hasfreq=1;
+hassignal=0;
+winx=175;
+winy=3.833333e+01;
+grafixwinx=3.603333e+02;
+grafixwiny=2.776667e+02;
+grafixwinb=560;
+grafixwinh=420;
--- matlab/auditorymodels/patterson/aim2006/Sounds/PT_200HZ/PT_200HZ_project.m
+++ matlab/auditorymodels/patterson/aim2006/Sounds/PT_200HZ/PT_200HZ_project.m
@@ -4,20 +4,20 @@
 
 %%%%%%%%%%%%%
 %   PT_200HZ
-%   26-Sep-2017
+%   19-Oct-2017
 %   produced by joy4u
 % Dont write anything in this file
 %%%%%%%%%%%%%
 current_plot=7;
-start_time=7;
+start_time=6;
 duration=0.040000;
 scale=1.000000;
 hastime=0;
 hasfreq=0;
 hassignal=0;
-winx=4.163333e+02;
-winy=223;
-grafixwinx=587;
-grafixwiny=3.023333e+02;
+winx=175;
+winy=3.833333e+01;
+grafixwinx=361;
+grafixwiny=1.796667e+02;
 grafixwinb=604;
 grafixwinh=3.666667e+02;
