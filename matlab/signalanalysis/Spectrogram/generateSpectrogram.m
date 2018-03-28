2c9952184f88a7bca746bcd3f34f7e00c374568c
joy4u18 <joy4u18@gmail.com>
2018-03-28 21:09:48 +0200

    Changed the spectrogram script
    Changed the spectrogram script to plot the noobject2, 50,200 in anechoic
    and conference room or 100,150 for lecture room

--- matlab/signalanalysis/Spectrogram/generateSpectrogram.m
+++ matlab/signalanalysis/Spectrogram/generateSpectrogram.m
@@ -1,71 +1,124 @@
 clc;
 clearvars;
 close all;
-%%
-
-%%
 %%  Locating and Initialzing the names for the .wav files
-T_d=500;
+
+T_d=5;
 cLow = -180;
 cHigh = 0;
-cd('C:\Users\joy4u\OneDrive\Joy\Research\Thesis\RECORDINGS\Exactduration_callibration\lecture\500ms\left_ear')
+roomnames = {'anechoic','conference','lecture'};
+ti={'A','B','C','D','E','F','G','H', 'I'};
 
-wavfile=dir('*.wav');          % To read the .wav files in the directory
-T_count=1;                     % Setting the counters
-T_count1=1;
-
-
-
-% ti={'NoObject rec1';'NoObject rec2';'Object at 50cm';...
-%     'Object at 100cm';'Object at 200cm';'Object at 300cm'; ...
-%     'Object at 400cm';'Object at 500cm'};
-
-% ti={'A','B','C','D','E','F','G','H'};
-
-
-% ti={'NoObject';'Object at 100cm';'Object at 150cm'};
- ti={'A','B','C'};
-
-T_mean_sc=0;
-
-
-
-for j=1:length(wavfile)
+T_acounter=1;
+T_ccounter=4;
+T_lcounter=7;
+for roomindex = 1:3
     
-    wavname=wavfile(j).name;      % Reading the wavefile names
-    
-%     hs=subplot(4,2,T_count1);
-    hs=subplot(2,2,T_count1);
-    T_count=T_count+1;
-    
-    [T_input, T_fs]= audioread(wavname);
-    
-    T_frametime=32e-3;
-    T_overlaptime=16e-3;
-    T_framelength=round(T_frametime*T_fs);
-    T_overlaplength=round(T_overlaptime*T_fs);
-    NFFT = 4096;
-   
-    spectrogram(T_input(1:round(((T_d+450)*T_fs*1e-3))),T_framelength,T_overlaplength,NFFT,T_fs,'yaxis')
-    caxis([cLow cHigh]);
-    colormap jet;
-    colorbar off;
-    
-    set(gca,'FontName','Arial');
-%     ht = text(hs,380+T_d,28,ti{1,T_count1},'FontWeight','bold','FontSize',12,'FontName','Arial');   
-    ht = text(hs,380+T_d,25,ti{1,T_count1},'FontWeight','bold','FontSize',12,'FontName','Arial');   
-    if (T_count>10)
-    T_count=1;    
-    T_count1 = T_count1+1;
-    try
-%     hs=subplot(4,2,T_count1);
-    hs=subplot(2,2,T_count1);
-    catch err
-    warning(err.message);    
-    end
+    String = string(roomnames(roomindex));
+    if(String == 'anechoic')
+        T_room = 1;
+        T_noobjectcount = 20;
+    elseif (String == 'conference')
+        T_room = 2;
+        T_noobjectcount = 20;
+    else
+        T_room = 3;
     end
     
-  
+    cd(['C:\Users\joy4u\OneDrive\Joy\Research\Thesis\RECORDINGS\Exactduration_callibration\' char(String) '\' char(string(T_d)) 'ms\left_ear'])
+    
+    wavfile=dir('*.wav');          % To read the .wav files in the directory
+    T_count =1;
+    
+    for j=1:length(wavfile)
+        
+        wavname=wavfile(j).name;      % Reading the wavefile names
+        if(T_room==1)
+            hs=subplot(3,3,T_acounter);
+        elseif(T_room==2)
+            hs=subplot(3,3,T_ccounter);
+        else
+            hs=subplot(3,3,T_lcounter);
+        end
+        
+        T_count=T_count+1;
+        
+        [T_input, T_fs]= audioread(wavname);
+        
+        T_frametime=32e-3;
+        T_overlaptime=16e-3;
+        T_framelength=round(T_frametime*T_fs);
+        T_overlaplength=round(T_overlaptime*T_fs);
+        NFFT = 4096;
+        
+        spectrogram(T_input(1:round(((T_d+450)*T_fs*1e-3))),T_framelength,T_overlaplength,NFFT,T_fs,'yaxis')
+        caxis([cLow cHigh]);
+        colormap jet;
+        colorbar off;
+        
+        set(gca,'FontName','Arial');
+        %     if(T_room ==1)
+        %         ht = text(hs,380+T_d,28,ti{1,T_count1},'FontWeight','bold','FontSize',12,'FontName','Arial');
+        %     else
+        %         ht = text(hs,380+T_d,25,ti{1,T_count1},'FontWeight','bold','FontSize',12,'FontName','Arial');
+        %     end
+        
+        if(T_room<3 && T_noobjectcount > 0) % this is to discard the first no object
+            T_noobjectcount = T_noobjectcount -1;
+            if (T_count>20)
+                T_count=1;
+                try
+                    if(T_room ==1)
+                        T_acounter = T_acounter+1;
+                        if(T_acounter == T_ccounter)
+                            break;
+                        end
+                        hs=subplot(3,3,T_acounter);
+                    elseif(T_room == 2)
+                        T_ccounter = T_ccounter+1;
+                        if(T_ccounter == T_lcounter)
+                            break;
+                        end
+                        hs=subplot(3,3,T_ccounter);
+                    else
+                        T_lcounter = T_lcounter+1;
+                        hs=subplot(3,3,T_lcounter);
+                    end
+                catch err
+                    warning(err.message);
+                end
+            end
+        else
+            if (T_count>10)
+                T_count=1;
+                try
+                    if(T_room ==1)
+                        T_acounter = T_acounter+1;
+                        T_count = -9; % this is to retrive object at 200  and discard 100 in the plot
+                        if(T_acounter == T_ccounter)
+                            break;
+                        end
+                        hs=subplot(3,3,T_acounter);
+                    elseif(T_room == 2)
+                        T_ccounter = T_ccounter+1;
+                        T_count = -9; % this is to retrive object at 200  and discard 100 in the plot
+                        if(T_ccounter == T_lcounter)
+                            break;
+                        end
+                        hs=subplot(3,3,T_ccounter);
+                    else
+                        T_lcounter = T_lcounter+1;
+                        hs=subplot(3,3,T_lcounter);
+                    end
+                catch err
+                    warning(err.message);
+                end
+            end
+        end
+        
+        
+    end
+    
 end
 %%
 
